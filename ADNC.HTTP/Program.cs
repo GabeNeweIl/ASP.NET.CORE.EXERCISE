0acf9c3865f14f6399dc4934364e9074be1db1f2
smeni0nik <smeni0nik@gmail.com>
2023-08-30 21:27:16 +0200

    Added validation and request handling

--- ADNC.HTTP/Program.cs
+++ ADNC.HTTP/Program.cs
@@ -1,70 +1,172 @@
-var builder = WebApplication.CreateBuilder(args);
-var app = builder.Build();
+using System.Net;
+using System.Reflection;
 
-app.Run(async (HttpContext context) =>
+internal class Program
 {
-    if (context.Request.Method == "GET")
+    private static void Main(string[] args)
     {
-        var queryParams = new QueryParams
+        var builder = WebApplication.CreateBuilder(args);
+        var app = builder.Build();
+
+        app.Run(async (HttpContext context) =>
         {
-            FirstNumber = context.Request.Query["firstNumber"],
-            SecondNumber = context.Request.Query["secondNumber"],
-            Operation = context.Request.Query["operation"]
+            if (context.Request.Method == "GET" && context.Request.Path == "/")
+            {
+                var queryParams = new QueryParams
+                {
+                    FirstNumber = context.Request.Query["firstNumber"],
+                    SecondNumber = context.Request.Query["secondNumber"],
+                    Operation = context.Request.Query["operation"]
+                };
+
+                var validationResults = Validation.ValidateParameters(queryParams);
+
+                if (validationResults.IsValid)
+                {
+                    var firstNumber = Convert.ToInt32(queryParams.FirstNumber);
+                    var secondNumber = Convert.ToInt32(queryParams.SecondNumber);
+                    switch (queryParams.Operation)
+                    {
+                        case "add":
+                            var result = firstNumber + secondNumber;
+                            await context.Response.WriteAsync(result.ToString());
+                            break;
+
+                        case "subtract":
+                            result = firstNumber - secondNumber;
+                            await context.Response.WriteAsync(result.ToString());
+                            break;
+
+                        case "multiply":
+                            result = firstNumber * secondNumber;
+                            await context.Response.WriteAsync(result.ToString());
+                            break;
+
+                        case "divide":
+                            if (Convert.ToInt32(queryParams.SecondNumber) != 0)
+                            {
+                                result = firstNumber / secondNumber;
+                                await context.Response.WriteAsync(result.ToString());
+                            }
+                            else
+                            {
+                                context.Response.StatusCode = Convert.ToInt32(HttpStatusCode.BadRequest);
+                                await context.Response.WriteAsync("Dividing by zero");
+                            }
+                            break;
+
+                        case "exact divide":
+                            if (Convert.ToInt32(queryParams.SecondNumber) != 0)
+                            {
+                                result = firstNumber % secondNumber;
+                                await context.Response.WriteAsync(result.ToString());
+                            }
+                            else
+                            {
+                                context.Response.StatusCode = Convert.ToInt32(HttpStatusCode.BadRequest);
+                                await context.Response.WriteAsync("Dividing by zero");
+                            }
+                            break;
+
+                        default:
+                            context.Response.StatusCode = Convert.ToInt32(HttpStatusCode.BadRequest);
+                            await context.Response.WriteAsync("Invalid input for 'operation'");
+                            break;
+                    }
+                }
+                else
+                {
+                    context.Response.StatusCode = Convert.ToInt32(HttpStatusCode.BadRequest);
+                    var body = string.Empty;
+
+                    foreach (var fieldName in validationResults.FieldNames)
+                    {
+                        body += $"Invalid input for '{fieldName}'\n";
+                    }
+
+                    await context.Response.WriteAsync(body);
+                }
+            }
+            else
+            {
+                context.Response.StatusCode = Convert.ToInt32(HttpStatusCode.BadRequest);
+                await context.Response.StartAsync();
+            }
+        });
+
+        app.Run();
+    }
+}
+
+public class Validation
+{
+    public static ValidationResults ValidateParameters(QueryParams queryParams)
+    {
+        var validationResults = new ValidationResults
+        {
+            FieldNames = new List<string>(),
+            IsValid = true
         };
-    }
-});
 
-static List<string> GetNullFields(object obj)
-{
-    if (obj == null)
-    {
-        throw new ArgumentNullException(nameof(obj));
-    }
-
-    var nullFields = new List<string>();
-
-    var type = obj.GetType();
-    foreach (var field in type.GetFields())
-    {
-        var value = field.GetValue(obj);
-        if (value == null)
+        try
         {
-            nullFields.Add(field.Name);
+            GetNullFields(queryParams, validationResults.FieldNames);
+            CheckTypeInt(queryParams, validationResults.FieldNames);
+        }
+        catch
+        {
+            validationResults.IsValid = false;
+            return validationResults;
+        }
+
+        return validationResults;
+    }
+
+    private static void GetNullFields(object obj, List<string> isValidQueryParams)
+    {
+        var type = obj.GetType();
+        var fields = type.GetRuntimeFields();
+
+        foreach (var field in fields)
+        {
+            var value = field.GetValue(obj);
+            if (value == null)
+            {
+                isValidQueryParams.Add(field.Name.Substring(1, field.Name.IndexOf(">") - 1));
+            }
+        }
+
+        if (isValidQueryParams.Any())
+        {
+            throw new Exception();
         }
     }
 
-    return nullFields;
-}
-
-static IsValidQueryParams IsValid(QueryParams queryParams, HttpContext context)
-{
-    var result = new IsValidQueryParams
+    private static void CheckTypeInt(QueryParams queryParams, List<string> isValidQueryParams)
     {
-        FieldNames = new List<string?>(),
-        IsValid = true
-    };
+        try
+        {
+            Convert.ToInt32(queryParams.FirstNumber);
+        }
+        catch
+        {
+            isValidQueryParams.Add(nameof(queryParams.FirstNumber));
+        }
 
-    var isNullFieldsResult = GetNullFields(queryParams);
+        try
+        {
+            Convert.ToInt32(queryParams.SecondNumber);
+        }
+        catch
+        {
+            isValidQueryParams.Add(nameof(queryParams.SecondNumber));
+        }
 
-    if (isNullFieldsResult is not null)
-    {
-        result.FieldNames = isNullFieldsResult;
-        result.IsValid = false;
-
-        return result;
+        if (isValidQueryParams.Any())
+        {
+            throw new Exception();
+        }
     }
-
-    try
-    {
-        var firstNumber = Convert.ToInt32(queryParams.FirstNumber);
-        var secondNumber = Convert.ToInt32(queryParams.SecondNumber);
-    }
-    catch (FormatException ex)
-    {
-        
-    }
-
-    return result;
 }
 
 public class QueryParams
@@ -76,9 +178,9 @@ public class QueryParams
     public string? Operation { get; set; }
 }
 
-public class IsValidQueryParams
+public class ValidationResults
 {
-    public List<string?> FieldNames { get; set; }
+    public List<string> FieldNames { get; set; }
 
     public bool IsValid { get; set; }
 }
